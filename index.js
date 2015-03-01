320e670466a6ec89c7a4cf5498de02f7ec4fff6b
Pedro.Pena <pedro.jose@gigigo.com>
2015-03-01 20:07:57 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var CONFIG  = require('./config.json');
+
+var dirProject = CONFIG[CONFIG.server].directory;
+var PORT = CONFIG[CONFIG.server].port;
+
+
+app.use('/app', express.static(__dirname + '/'+dirProject+'/app'));
+app.use('/assets', express.static(__dirname + '/'+dirProject+'/assets'));
+
+app.all('/*', function(req, res, next) {
+    res.sendfile('index.html', {root: __dirname + '/' + dirProject + '/'});
+});
+
+http.createServer(app).listen(PORT);
+console.log('server listen'+ PORT);
